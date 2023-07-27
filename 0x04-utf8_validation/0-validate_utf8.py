3aabc01469d5278445d7811e190db351d46d1a12
CheropS <sh.cherop@gmail.com>
2023-07-27 10:41:04 +0300

    fix errors

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""valid UTF8"""
+"""validUTF8"""
 
 
 def validUTF8(data):
@@ -10,17 +10,17 @@ def validUTF8(data):
     byte2 = 1 << 6
 
     for a in data:
-        z = 1 << 7
+        m = 1 << 7
         if nbytes == 0:
-            while z & a:
+            while m & a:
                 nbytes += 1
-                z = z >> 1
-                if nbytes == 0:
-                    continue
-                if nbytes == 1 or nbytes > 4:
-                    return False
-                else:
-                    if not (a & byte1 and not (a & byte2)):
-                        return False
-                    nbytes -= 1
-            return nbytes == 0
+                m = m >> 1
+            if nbytes == 0:
+                continue
+            if nbytes == 1 or nbytes > 4:
+                return False
+        else:
+            if not (a & byte1 and not (a & byte2)):
+                return False
+        nbytes -= 1
+    return nbytes == 0
